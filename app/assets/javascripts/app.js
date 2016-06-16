b776f281cbe9192858d645a69c63e99673ffdd63
Kirill Zonov <graffzon@gmail.com>
2016-06-16 20:50:50 +0300

    removed outdated test code

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -12,13 +12,6 @@
   app.controller('HomeController', ['$scope', '$auth', '$state', HomeController]);
 
   function HomeController($scope, $auth, $state) {
-    $scope.counter = 1;
-    $scope.plusOne = function() {
-      $scope.counter = $scope.counter + 1;
-    };
-    $scope.greet = function() {
-      alert($scope.counter);
-    };
     $scope.isLoggedIn = function() {
       return $auth.userIsAuthenticated();
     };
