a23b822498bf24197b92a2d579cc4ef15735c612
Kirill Zonov <graffzon@gmail.com>
2016-06-22 10:11:17 +0300

    added state field for appointment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622064813) do
+ActiveRecord::Schema.define(version: 20160622071000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160622064813) do
     t.integer  "creator_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "state"
   end
 
   add_index "appointments", ["creator_id"], name: "index_appointments_on_creator_id", using: :btree
